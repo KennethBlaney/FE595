abfe21e6ab23c1bd81bf5f1eef6420ed9c5dad27
KennethBlaney <kenneth.r.blaney@gmail.com>
2019-02-07 16:16:06 -0500

    hello world

--- run_hello.py
+++ run_hello.py
@@ -2,10 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/<name>', methods=['GET'])
-def hello_world(name):
-    return "Hello, {}!".format(name)
+@app.route('/hello', methods=['GET'])
+def hello_world():
+    return "Hello, world!"
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
+    
