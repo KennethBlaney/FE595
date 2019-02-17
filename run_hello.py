0761c0c08a161ae7d3a94adcfb3aed60000284c7
kblaney <kennethrblaney@gmail.com>
2019-02-17 13:33:46 -0500

    change template

--- run_hello.py
+++ run_hello.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
     print("what should I print?")
@@ -15,4 +16,3 @@ def hello_person(name):
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
-    
