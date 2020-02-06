fbe61a91ca43bccf1ff3ecf4026902d0bad6a6cd
KennethBlaney <kenneth.r.blaney@gmail.com>
2020-02-05 19:53:27 -0500

    add html

--- run_hello.py
+++ run_hello.py
@@ -10,6 +10,17 @@ def hello_world():
 def hello_name(name):
     return f"Hello, {name}"
 
+@app.route('/interesting/<name>', methods=['GET'])
+def serve_html(name):
+    return f"""
+    <html>
+    <body>
+    <h1>Hello {name}</h1>
+    <p>This is my fancy webpage</p>
+    </body>
+    </html>
+    """
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
