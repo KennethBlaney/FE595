17e59dc4560aa30d280e5dead2165cdd415124a4
KennethBlaney <kenneth.r.blaney@gmail.com>
2020-02-05 19:45:00 -0500

    add variable

--- run_hello.py
+++ run_hello.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, world!"
 
+@app.route('/hello/<name>', methods=['GET'])
+def hello_name(name):
+    return f"Hello, {name}"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
